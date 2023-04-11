08e1f03c88f53b38531c4788975c946851526767
Mahadi90 <mahadimhs787@gmail.com>
2023-04-11 14:28:15 +0600

    header and home resposive added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import {
+    BoltIcon,
+    Bars3BottomRightIcon,
+    XMarkIcon,
+  } from '@heroicons/react/24/solid'
 
 const Header = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState()
+
     return (
         <nav className="flex justify-between py-8 px-2 md:px-14 bg-gradient-to-r from-gray-300 to-gray-100">
             <div>
                 <h2 className='text-3xl font-bold'><span className='text-violet-600'>MR</span>jobHub</h2>
             </div>
-            <ul>
+            <ul className='hidden lg:flex'>
                 <Link className='text-violet-600 mx-6' to='/'>Home</Link>
                 <Link className='text-violet-600 mx-6' to='/statistics'>Statistics</Link>
                 <Link className='text-violet-600 mx-6' to='/appliedjob'>Applied Job</Link>
                 <Link className='text-violet-600 mx-6' to='/blog'>Blog</Link>
             </ul>
-            <button className='common-btn'>Start Applying</button>
+            <div className='lg:hidden'>
+          {/* Dropdown Open Button */}
+          <button
+            aria-label='Open Menu'
+            title='Open Menu'
+            onClick={() => setIsMenuOpen(true)}
+          >
+            <Bars3BottomRightIcon className='w-8 text-violet-600' />
+          </button>
+          {isMenuOpen && (
+            <div className='absolute top-0 left-0 w-full z-10'>
+              <div className='p-5 bg-white border rounded shadow-sm'>
+                {/* Logo & Button section */}
+                <div className='flex items-center justify-between mb-4'>
+                  <div>
+                    <Link to='/' className='inline-flex items-center'>
+                      <span className='ml-2 text-xl font-bold tracking-wide text-gray-800 uppercase'>
+                        <span className='text-violet-500'>MR</span>jobHub
+                      </span>
+                    </Link>
+                  </div>
+                  {/* Dropdown menu close button */}
+                  <div>
+                    <button
+                      aria-label='Close Menu'
+                      title='Close Menu'
+                      onClick={() => setIsMenuOpen(false)}
+                    >
+                      <XMarkIcon className='w-5 text-gray-600' />
+                    </button>
+                  </div>
+                </div>
+                {/* Mobile Nav Items Section */}
+                <nav>
+                  <ul className='space-y-4'>
+                    <li>
+                      <Link to='/' className='default'>
+                        Home
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to='/statistics'
+                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                      >
+                        Statistics
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to='/appliedjob'
+                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                      >
+                        Applied Job
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to='/blog'
+                        className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-blue-400'
+                      >
+                        Blog
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+          )}
+        </div>
+            <button className='common-btn hidden lg:flex'>Start Applying</button>
         </nav>
     );
 };
